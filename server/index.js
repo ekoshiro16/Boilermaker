d3fb13582add9a350b552035b509c700ecf9ec6e
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-07 02:03:16 -0700

    send index HTML

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
 // Entry point for server javascript
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express(); 
+
+// Logger middleware
+app.use(morgan('dev')); 
+
+// Static middleware
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// api routes
+app.use('/api', require('./apiRoutes'));
+
+// send HTML
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
