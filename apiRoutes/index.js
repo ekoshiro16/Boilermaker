6e645c3ef66fb77ee61e06f2d39299c9cfe4e048
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-07 01:40:32 -0700

    handle 404s

--- apiRoutes/index.js
+++ apiRoutes/index.js
@@ -5,4 +5,10 @@ router.use('/villagers', require('./villagers'));
 router.use('/houses', require('./houses'));
 router.use('/islands', require('./islands'));
 
+router.use(function (req, res, next) {
+    const err = new Error('Not found.');
+    err.status = 404;
+    next(err);
+  });
+
 module.exports = router;
