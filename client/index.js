663703529d58607f5257669ed0c3defef5cc1092
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-07 00:58:33 -0700

    app created and logger middleware

--- client/index.js
+++ client/index.js
@@ -1,1 +1,10 @@
 // Entry point for client Javascript
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express(); 
+
+
+// Logger middleware
+app.use(morgan('dev'));
