8545cd8f6eb14d70a2fa826b1a286756ce0d71a0
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-07 03:23:09 -0700

    ReactDOM render

--- client/index.js
+++ client/index.js
@@ -1,1 +1,9 @@
 // Entry point for client Javascript
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>Hello, world!</div>,
+    document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+  );
